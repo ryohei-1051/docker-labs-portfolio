e9ee7d3c59495145ccf5b6c9ec0767ebf659e310
Ryohei Nakao <ryohei19891019@gmail.com>
2026-04-06 10:54:27 +0900

    Update deploy-service.sh

--- lab02-docker-swarm-httpd/scripts/deploy-service.sh
+++ lab02-docker-swarm-httpd/scripts/deploy-service.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
 
+IMAGE_NAME="${1:-ryohei-httpd:latest}"
+SERVICE_NAME="${2:-ryohei-web}"
+PUBLISHED_PORT="${3:-8080}"
+
+docker service create \
+  --name "$SERVICE_NAME" \
+  --replicas 3 \
+  --publish "${PUBLISHED_PORT}:80" \
+  "$IMAGE_NAME"
+
+docker service ps "$SERVICE_NAME"
