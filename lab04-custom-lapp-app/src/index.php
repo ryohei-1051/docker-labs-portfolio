980976bda43d7dae500cda4cba696041618a0f9b
Ryohei Nakao <ryohei19891019@gmail.com>
2026-04-06 11:01:39 +0900

    Update index.php

--- lab04-custom-lapp-app/src/index.php
+++ lab04-custom-lapp-app/src/index.php
@@ -1,1 +1,19 @@
+<?php
+$yourname = "Ryohei Nakao";
+$yourstudentnumber = "A01305924";
+$image = "docker.jpg";
+$uname = php_uname();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title><?php echo $yourname . ' - ' . $yourstudentnumber; ?></title>
+</head>
+<body>
+    <h1><?php echo $yourname . ' - ' . $yourstudentnumber; ?></h1>
+    <img src="/<?php echo $image; ?>" alt="Custom lab image" width="400" />
+    <div><?php echo $uname; ?></div>
+</body>
+</html>
 
